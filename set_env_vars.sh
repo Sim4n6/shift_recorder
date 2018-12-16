429c94bd4a06512c899225a97347743f5d28cb99
Sim4n6 <sim4n6@gmail.com>
2018-12-16 22:22:19 +0100

    - added shift_add template - added message flashed in login_form template - added support for database connection, insertion and query - added a script to start server and set env variables

---
+++ set_env_vars.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=shift_recorded_flask.py
+export FLASK_ENV=development
+flask run
--- shift_recorded_flask.py
+++ shift_recorded_flask.py
@@ -1,34 +1,113 @@
-from flask import Flask, url_for, redirect, request, render_template
+from flask import Flask, url_for, redirect, request, render_template, flash, session
+import xlsxwriter as xwr
+import sqlite3 as lite
+from hashlib import sha1
+
+
+def encrypt_password(password):
+    return sha1(password.encode('UTF-8')).hexdigest()
+
+
+def initialize_db(db_name):
+    try:
+        conn0 = lite.connect(db_name)
+        c0 = conn0.cursor()
+        c0.execute('''CREATE TABLE IF NOT EXISTS user(employee_id INTEGER PRIMARY KEY, username TEXT NOT NULL, 
+                    name TEXT NOT NULL, password TEXT NOT NULL );''')
+        conn0.commit()
+    except lite.Error as e:
+        conn0.rollback()
+        app.logger.error("An error occurred : " + e.args[0])
+    finally:
+        conn0.close()
+
+
+def add_new_user(db_name):
+    try:
+        conn4 = lite.connect(db_name)
+        c4 = conn4.cursor()
+        # Find Existing username if any take proper action
+        find_user = '''SELECT DISTINCT username, name FROM user WHERE username = ? and name = ? '''
+        c4.execute(find_user, [request.form["username"], request.form["name"]])
+        if c4.fetchall():
+            flash('Username taken try a different one, please.')
+        else:
+            # defined a function for encrypting password
+            conn4.create_function('encrypt', 1, encrypt_password)
+            # Create New Account
+            insert = '''INSERT INTO user (username, name, password) VALUES(?, ?, encrypt(?))'''
+            c4.execute(insert, [request.form["username"], request.form["name"], request.form["password"]])
+            conn4.commit()
+    except lite.Error as e:
+        conn4.rollback()
+        app.logger.error("An error occurred : " + e.args[0])
+    finally:
+        conn4.close()
+
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8zZERUNdnJE'
 
 
 @app.route('/')
 @app.route('/index')
-@app.route('/login')
-def login():
-    return render_template("login_form.html")
+def index():
+    initialize_db("shifts.db")
+    if 'username' in session:
+        return redirect(url_for("shift_add"))
+    return redirect(url_for("login_check"))
 
 
-@app.route('/login_check', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login_check():
     if request.method == 'POST':
-        app.logger.debug("a request has been made from client to server : asking to POST some data :\n" + "username : ")
-        app.logger.debug(request.form["username"] + " - " + request.form["password"] + "button: " + request.form["submit_btn"])
         if request.form["submit_btn"] == "new user":
             return redirect(url_for("create_new_user"))
         else:
-            return "ok data to be checked: " + request.form["username"] + " ... " + request.form["password"]
+            try:
+                # Establish Connection
+                conn3 = lite.connect('shifts.db')
+                c3 = conn3.cursor()
+                # Find user If there is any take proper action
+                find_user = '''SELECT * FROM user WHERE username = ? and password = ?'''
+                c3.execute(find_user, [request.form["username"], encrypt_password(request.form["password"])])
+                result = c3.fetchall()
+
+                if result:
+                    session['username'] = request.form['username']
+                    return redirect(url_for("shift_add"), request.form["username"])
+                else:
+                    flash('could not connect !')
+            except lite.Error as e:
+                conn3.rollback()
+                app.logger.error("An error occurred : ", e.args[0])
+            finally:
+                conn3.close()
+            return redirect(url_for("login_check"))
+    elif request.method == 'GET':
+        return render_template("login_form.html")
     else:
-        print("a requestfor a GET made")
         return "a request has been made from a client to server : asking for something"
 
 
-@app.route('/create_new_user_check', methods=['GET', 'POST'])
+@app.route('/create_new_user', methods=['GET', 'POST'])
 def create_new_user():
     if request.method == 'GET':
         return render_template("new_user_form.html")
-    elif request.method  == 'POST':
-        return "create new user with data: " + request.form["name"]
+    elif request.method == 'POST':
+        add_new_user("shifts.db")
+        flash(f"a user named : {request.form['name']} has been created")
+        return redirect(url_for("login_check"))
     else:
         return "else request method "
+
+
+@app.route("/shift_add")
+def shift_add():
+    return render_template("shift_add.html")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
